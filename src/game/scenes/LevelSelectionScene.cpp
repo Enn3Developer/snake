40b591b2fe734fb908191d18855f1588c1ef905c
Enn3Developer <arturpeshko39@gmail.com>
2025-06-10 19:18:01 +0200

    added game initialization based on difficulty

--- src/game/scenes/LevelSelectionScene.cpp
+++ src/game/scenes/LevelSelectionScene.cpp
@@ -1,8 +1,51 @@
 #include "LevelSelectionScene.h"
 
+#include "GameScene.h"
 #include "MainScene.h"
 
 MainScene *main_scene_from_level_selection;
+GameScene *game_scene_from_level_selection;
+
+void startGame(RunContext *ctx, int level) {
+    int speed = 2;
+    int length = 2;
+    int bonus = 1;
+
+    switch (level) {
+        case 1:
+            speed = 7;
+            length = 6;
+            bonus = 1;
+            break;
+        case 2:
+            speed = 5;
+            length = 10;
+            bonus = 3;
+            break;
+        case 3:
+            speed = 3;
+            length = 15;
+            bonus = 7;
+            break;
+        default: break;
+    }
+
+    game_scene_from_level_selection = new GameScene(length, speed, level);
+    game_scene_from_level_selection->setBonusPoints(bonus);
+    ctx->queueScene(game_scene_from_level_selection);
+}
+
+void onEasy(RunContext *ctx) {
+    startGame(ctx, 1);
+}
+
+void onNormal(RunContext *ctx) {
+    startGame(ctx, 2);
+}
+
+void onHard(RunContext *ctx) {
+    startGame(ctx, 3);
+}
 
 LevelSelectionScene::LevelSelectionScene() {
     this->easy = new Button();
@@ -22,10 +65,13 @@ LevelSelectionScene::LevelSelectionScene() {
     this->hard->setNormalColor(ColorPair(COLOR_RED, COLOR_BLACK));
     this->hard->setHoverColor(ColorPair(COLOR_RED, COLOR_WHITE));
 
-    // impostazione della posizione nella scena
     this->easy->setPosition(getCenteredX(this->easy), 8);
     this->normal->setPosition(getCenteredX(this->normal), 11);
     this->hard->setPosition(getCenteredX(this->hard), 14);
+
+    this->easy->setOnClick(onEasy);
+    this->normal->setOnClick(onNormal);
+    this->hard->setOnClick(onHard);
 
     this->add(this->easy);
     this->add(this->normal);
